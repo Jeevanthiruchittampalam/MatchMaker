404f8465f52532d57a220478ec3edb9efc13437f
Jeevan Thiruchittampalam <jeevanthiru02@hotmail.com>
2024-05-01 12:40:21 -0700

    Sign up vs Sign in

--- lib/widgets/loginscreen.dart
+++ lib/widgets/loginscreen.dart
@@ -27,7 +27,7 @@ class _LoginScreenState extends State<LoginScreen> {
         child: Stack(
           children: [
             Positioned(
-              top: MediaQuery.of(context).size.height * 0.1, // Positions the welcome text higher
+              top: MediaQuery.of(context).size.height * 0.1,
               left: 0,
               right: 0,
               child: const Text(
@@ -45,7 +45,7 @@ class _LoginScreenState extends State<LoginScreen> {
               child: Padding(
                 padding: const EdgeInsets.all(24.0),
                 child: Column(
-                  mainAxisSize: MainAxisSize.min, // Aligns the column at the bottom of the screen
+                  mainAxisSize: MainAxisSize.min,
                   children: [
                     TextField(
                       controller: _emailController,
@@ -77,15 +77,23 @@ class _LoginScreenState extends State<LoginScreen> {
                     ElevatedButton(
                       onPressed: _login,
                       style: ElevatedButton.styleFrom(
-                        backgroundColor: Color.fromARGB(255, 6, 3, 120), // Button color.
+                        backgroundColor: Color.fromARGB(255, 6, 3, 120),
                         padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 15),
                         shape: RoundedRectangleBorder(
                           borderRadius: BorderRadius.circular(30.0),
                         ),
                       ),
                       child: const Text(
-                        'Login / Sign Up',
+                        'Login',
                         style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    TextButton(
+                      onPressed: _createUser,
+                      child: const Text(
+                        'Sign Up',
+                        style: TextStyle(color: Colors.blue),
                       ),
                     ),
                   ],
@@ -99,45 +107,44 @@ class _LoginScreenState extends State<LoginScreen> {
   }
 
   void _login() async {
-  try {
-    // Try to sign in with the provided credentials.
-    UserCredential userCredential = await _auth.signInWithEmailAndPassword(
-      email: _emailController.text.trim(),
-      password: _passwordController.text.trim(),
-    );
-    Navigator.pushReplacement(
-      context,
-      MaterialPageRoute(builder: (context) => const MenuScreen()),
-    );
-  } on FirebaseAuthException catch (e) {
-    if (e.code == 'user-not-found' || e.code == 'wrong-password') {
-      // No user found or wrong password, you might want to create a new user or ask for retry
-      print('No user found or wrong password.');
-      _createUser(); // Call a function to handle sign up
-    } else {
-      print('Error: ${e.message}');
+    try {
+      UserCredential userCredential = await _auth.signInWithEmailAndPassword(
+        email: _emailController.text.trim(),
+        password: _passwordController.text.trim(),
+      );
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => const MenuScreen()),
+      );
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'user-not-found') {
+        _showError('No user found for that email. Please sign up.');
+      } else if (e.code == 'wrong-password') {
+        _showError('Wrong password provided. Please try again.');
+      } else {
+        _showError('Login Error: ${e.message}');
+      }
     }
   }
-}
 
-void _createUser() async {
-  try {
-    // Create a new user with the provided credentials.
-    UserCredential newUser = await _auth.createUserWithEmailAndPassword(
-      email: _emailController.text.trim(),
-      password: _passwordController.text.trim(),
-    );
-    Navigator.pushReplacement(
-      context,
-      MaterialPageRoute(builder: (context) => const MenuScreen()),
-    );
-  } on FirebaseAuthException catch (e) {
-    print('Failed to create a new user: ${e.message}');
-    // Show error message to user
+  void _createUser() async {
+    try {
+      UserCredential newUser = await _auth.createUserWithEmailAndPassword(
+        email: _emailController.text.trim(),
+        password: _passwordController.text.trim(),
+      );
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => const MenuScreen()),
+      );
+    } on FirebaseAuthException catch (e) {
+      _showError('Failed to create a new user: ${e.message}');
+    }
+  }
+
+  void _showError(String message) {
     ScaffoldMessenger.of(context).showSnackBar(
-      SnackBar(content: Text('Failed to create a new user: ${e.message}'))
+      SnackBar(content: Text(message))
     );
   }
-}
-
 }
